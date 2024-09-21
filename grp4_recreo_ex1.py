e4edcca49587c5e4950b61618e5d9cd0ffcaadda
trisharecreo <143807398+TrishaRecreo@users.noreply.github.com>
2024-09-21 23:30:39 +0800

    Create grp4_recreo_ex1.py

---
+++ grp4_recreo_ex1.py
@@ -0,0 +1,41 @@
+# Comments
+# a. Create a simple print script
+# b. Add comment "Introduction Script"
+
+# Introduction Script
+print("Hello, I'm Trisha Recreo! BSCS 3G")
+
+# Variables
+# a. Create two variables and assign number and name on it.
+age = 21
+student_name = "Trisha Recreo"
+
+# b. Then print both values
+print("Age:", age)
+print("Student Name:", student_name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+Jewel = "Jewel"
+Jayson= "Jayson"
+Kyla = "Kyla"
+Jenny = "Jenny"
+
+# Print the values of the group member variables
+print("Group Member Names:")
+print("Jewel:",Jewel)
+print("Jayson:",Jayson)
+print("Kyla:",Kyla)
+print("Jenny:", Jenny)
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+length, width, height = 200, 100, 50
+print("Length:", length)
+print("Width:", width)
+print("Height:", height)
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+hobby1 = hobby2 = hobby3 = "Playing Basketball"
+print("Common Hobbies:", hobby1, hobby2, hobby3)
